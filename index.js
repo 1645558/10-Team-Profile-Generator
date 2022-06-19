b33e983a07d3f1018d3d3a3c12412f967321f472
Rare Raptor <connorwtuncc@gmail.com>
2022-06-19 14:24:23 -0400

    made functions for the questions to tie it all together when running prompt

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const managerQuestions = () => {
         {
             type: 'input',
             message: 'Please enter your name',
-            name: 'manager-name',
+            name: 'name',
         },
         {
             type: 'input',
@@ -28,67 +28,73 @@ const managerQuestions = () => {
             message: 'Please enter your office number',
             name: 'office',
         },
+    ])
+        .then(managerInput => {
+            const { name, id, email, officeNumber } = managerInput;
+            const manager = new Manager(name, id, email, officeNumber);
+
+            employeeArray.push(manager);
+            console.log(manager);
+        });
+};
+
+const addEmployee = () => {
+    return inquirer.prompt([
         {
             type: 'list',
             message: 'Do you want to add an employee?',
-            name: 'add-employee',
+            name: 'addEmployee',
             choices: [
                 'Engineer',
-                'Intern'
+                'Intern',
             ]
         },
-    ]);
+        {
+            type: 'input',
+            message: 'Please enter your employees name',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'Please enter your employees id',
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: 'Please enter your employees email address',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'Please enter your employees school',
+            name: 'school',
+        },
+        {
+            type: 'input',
+            message: 'Please enter your employees Github username',
+            name: 'github',
+        },
+        {
+            type: 'confirm',
+            message: 'Would you like to add more employees?',
+            name: 'confirmation',
+        }
+    ])
+        .then(employees => {
+            let { name, id, email, addEmployee, github, school, confirmation } = employees;
+            let employee;
+
+            if (addEmployee === 'Intern') {
+                employee = new Intern(name, id, email, school);
+                console.log(employee)
+            } else if (addEmployee === 'Engineer') {
+                employee = new Engineer(name, id, email, github);
+            };
+
+            if (confirmation) {
+                return addEmployee(employeeArray)
+            } else {
+                return employeeArray
+            };
+        });
 };
-
-const internQuestions = [
-    {
-        type: 'input',
-        message: 'Please enter your name',
-        name: 'intern-name',
-    },
-    {
-        type: 'input',
-        message: 'Please enter your id',
-        name: 'id',
-    },
-    {
-        type: 'input',
-        message: 'Please enter your email address',
-        name: 'email',
-    },
-    {
-        type: 'input',
-        message: 'Please enter your school',
-        name: 'school',
-    },
-];
-
-const engineerQuestions = [
-    {
-        type: 'input',
-        message: 'Please enter your name',
-        name: 'engineer-name',
-    },
-    {
-        type: 'input',
-        message: 'Please enter your id',
-        name: 'id',
-    },
-    {
-        type: 'input',
-        message: 'Please enter your email address',
-        name: 'email',
-    },
-    {
-        type: 'input',
-        message: 'Please enter your Github username',
-        name: 'github',
-    },
-];
-
-
-function init() {
-    inquirer.prompt(managerQuestions, internQuestions, engineerQuestions)
-};
-
-init();
